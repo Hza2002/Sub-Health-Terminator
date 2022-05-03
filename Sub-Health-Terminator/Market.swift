87a67b5fc52ba4f72055499265688280a8b7a79e
Ziang <2787086708@qq.com>
2022-05-04 01:49:35 +0800

    version 1.0

--- Sub-Health-Terminator/Market.swift
+++ Sub-Health-Terminator/Market.swift
@@ -1,5 +1,20 @@
 import SwiftUI
 
+// 样式
+struct PCGroupBoxStyle: GroupBoxStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        VStack(alignment: .leading) {
+            configuration.label
+                .font(.title)
+                .foregroundColor(.teal)
+            configuration.content
+        }
+        .padding()
+        .background(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+    }
+}
+
 // 搜索框
 struct Search : View {
     @State private var searchText: String = ""
@@ -22,11 +37,248 @@ struct Search : View {
     }
 }
 
+// 商品分类 8大板块
+struct Goods: View {
+    var body: some View {
+        GroupBox {
+            VStack {
+                // 第一排
+                HStack{
+                    Spacer()
+                        .frame(width: 10)
+                    VStack {
+                        Image("健康书籍")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 60, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 1))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5).stroke(.teal, lineWidth:1)
+                            )
+                            .shadow(radius: 1)
+                        Text("健康书籍")
+                            .foregroundColor(.teal)
+                            .font(.system(size: 15))
+                            .multilineTextAlignment(.center)
+                    }
+                    Spacer()
+                        .frame(width: 20)
+
+
+                    VStack {
+                        Image("保健食品")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 60, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 1))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5).stroke(.teal, lineWidth:1)
+                            )
+                            .shadow(radius: 1)
+                        Text("保健食品")
+                            .foregroundColor(.teal)
+                            .font(.system(size: 15))
+                            .multilineTextAlignment(.center)
+                    }
+                    Spacer()
+                        .frame(width: 20)
+
+                    VStack {
+                        Image("食品速递")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 60, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 1))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5).stroke(.teal, lineWidth:1)
+                            )
+                            .shadow(radius: 1)
+                        Text("食品速递")
+                            .foregroundColor(.teal)
+                            .font(.system(size: 15))
+                            .multilineTextAlignment(.center)
+                    }
+                    Spacer()
+                        .frame(width: 20)
+
+                    VStack {
+                        Image("健身设备")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 60, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 1))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5).stroke(.teal, lineWidth:1)
+                            )
+                            .shadow(radius: 1)
+                        Text("健身设备")
+                            .foregroundColor(.teal)
+                            .font(.system(size: 15))
+                            .multilineTextAlignment(.center)
+                    }
+                    Spacer()
+                        .frame(width: 10)
+                }
+
+                // 第二排
+                HStack{
+                    Spacer()
+                        .frame(width: 10)
+                    VStack {
+                        Image("定制食谱")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 60, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 1))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5).stroke(.teal, lineWidth:1)
+                            )
+                            .shadow(radius: 1)
+                        Text("定制食谱")
+                            .foregroundColor(.teal)
+                            .font(.system(size: 15))
+                            .multilineTextAlignment(.center)
+                    }
+                    Spacer()
+                        .frame(width: 20)
+
+
+                    VStack {
+                        Image("健身方案")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 60, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 1))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5).stroke(.teal, lineWidth:1)
+                            )
+                            .shadow(radius: 1)
+                        Text("健身方案")
+                            .foregroundColor(.teal)
+                            .font(.system(size: 15))
+                            .multilineTextAlignment(.center)
+                    }
+                    Spacer()
+                        .frame(width: 20)
+
+                    VStack {
+                        Image("积分兑换")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 60, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 1))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5).stroke(.teal, lineWidth:1)
+                            )
+                            .shadow(radius: 1)
+                        Text("积分兑换")
+                            .foregroundColor(.teal)
+                            .font(.system(size: 15))
+                            .multilineTextAlignment(.center)
+                    }
+                    Spacer()
+                        .frame(width: 20)
+
+                    VStack {
+                        Image("抽奖中心")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 60, height: 50)
+                            .clipShape(RoundedRectangle(cornerRadius: 1))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5).stroke(.teal, lineWidth:1)
+                            )
+                            .shadow(radius: 1)
+                        Text("抽奖中心")
+                            .foregroundColor(.teal)
+                            .font(.system(size: 15))
+                            .multilineTextAlignment(.center)
+                    }
+                    Spacer()
+                        .frame(width: 10)
+                }
+            }
+
+        } label: {
+            Label("商品分类", systemImage: "giftcard")
+        }
+        .padding(.vertical, -10.0)
+        .groupBoxStyle(PCGroupBoxStyle())
+    }
+}
+
+
+
+// 新品速递
+struct NewGoods: View {
+    var body: some View {
+        GroupBox {
+            HStack {
+                VStack{
+                    Image("平衡车")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 131, height: 75)
+                        .clipShape(RoundedRectangle(cornerRadius: 1))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 1).stroke(.teal, lineWidth:1)
+                        )
+                        .shadow(radius: 1)
+                    VStack {
+                        Text("平衡车")
+                            .foregroundColor(.teal)
+                            .font(.system(size: 15))
+                            .multilineTextAlignment(.center)
+                        Text("¥ 2199")
+                            .foregroundColor(.teal)
+                            .font(.system(size: 15))
+                            .multilineTextAlignment(.center)
+                    }
+
+                }
+                Spacer()
+                    .frame(width: 45 )
+                VStack{
+                    Image("心理丛书")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 131, height: 75)
+                        .clipShape(RoundedRectangle(cornerRadius: 1))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 1).stroke(.teal, lineWidth:1)
+                        )
+                        .shadow(radius: 1)
+                    VStack {
+                        Text("心理丛书")
+                            .foregroundColor(.teal)
+                            .font(.system(size: 15))
+                            .multilineTextAlignment(.center)
+                        Text("¥ 199")
+                            .foregroundColor(.teal)
+                            .font(.system(size: 15))
+                            .multilineTextAlignment(.center)
+                    }
+
+                }
+            }
+
+        } label: {
+            Label("新品速递", systemImage: "lightbulb")
+        }
+        .padding(.vertical, -11.0)
+        .groupBoxStyle(PCGroupBoxStyle())
+     }
+}
+
+
+
 struct Market: View {
     var body: some View{
         VStack{
             // 搜索框
             Search()
+            Spacer()
+                .frame(height: 20)
 
             // 商品大图
             Image("Market Main")
@@ -35,9 +287,16 @@ struct Market: View {
                 .clipShape(Rectangle())
                 .shadow(radius: 10)
                 .cornerRadius(/*@START_MENU_TOKEN@*/9.0/*@END_MENU_TOKEN@*/)
-                .frame(width: 380)
+                .frame(width: 350)
+            Spacer()
+                .frame(height: 10)
+
 
             // 商品分类 8大板块
+            Goods()
+
+            // 新品速递
+            NewGoods()
 
         }
     }
--- Sub-Health-Terminator/Mine.swift
+++ Sub-Health-Terminator/Mine.swift
@@ -1,52 +1,23 @@
 import SwiftUI
 
-struct Mine: View {
+struct Settings : View {
     var body: some View {
-
         VStack{
-
-            // 用户选项
-            VStack{
-                // 标题
-                Text("我的")
-                    .font(.largeTitle)
-                    .foregroundColor(.white)
-
-                // 头像框
-                HStack{
-                    Image("icon")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 50, height: 50)
-                        .clipShape(Circle())
-                        .overlay(
-                            Circle().stroke(.cyan, lineWidth: 4)
-                        )
-                        .shadow(radius: 10)
-                    VStack{
-                        Text("Adiministor")
-                            .foregroundColor(.white)
-                            .font(.system(size: 40))
-                    }
-                }
-            }
-
-
             // 我的钱包
             Button {
             } label: {
                 Image(systemName: "wallet.pass")
                     .foregroundColor(.teal)
-                    .font(.system(size: 40))
+                    .font(.system(size: 30))
                 Text("我的钱包")
                     .foregroundColor(.teal)
-                    .font(.system(size: 30))
+                    .font(.system(size: 25))
                     .multilineTextAlignment(.leading)
                 Spacer()
-                    .frame(width: 150)
+                    .frame(width: 180)
                 Image(systemName: "chevron.forward")
                     .foregroundColor(.teal)
-                    .font(.system(size: 30))
+                    .font(.system(size: 25))
             }
             .frame(height: 50)
 
@@ -56,16 +27,16 @@ struct Mine: View {
             } label: {
                 Image(systemName: "mappin.and.ellipse")
                     .foregroundColor(.teal)
-                    .font(.system(size: 40))
+                    .font(.system(size: 30))
                 Text("我的地址")
                     .foregroundColor(.teal)
-                    .font(.system(size: 30))
+                    .font(.system(size: 25))
                     .multilineTextAlignment(.leading)
                 Spacer()
-                    .frame(width: 150)
+                    .frame(width: 180)
                 Image(systemName: "chevron.forward")
                     .foregroundColor(.teal)
-                    .font(.system(size: 30))
+                    .font(.system(size: 25))
             }
             .frame(height: 50)
 
@@ -75,16 +46,16 @@ struct Mine: View {
             } label: {
                 Image(systemName: "giftcard")
                     .foregroundColor(.teal)
-                    .font(.system(size: 40))
+                    .font(.system(size: 30))
                 Text("我的积分")
                     .foregroundColor(.teal)
-                    .font(.system(size: 30))
+                    .font(.system(size: 25))
                     .multilineTextAlignment(.leading)
                 Spacer()
-                    .frame(width: 150)
+                    .frame(width: 180)
                 Image(systemName: "chevron.forward")
                     .foregroundColor(.teal)
-                    .font(.system(size: 30))
+                    .font(.system(size: 25))
             }
             .frame(height: 50)
 
@@ -93,16 +64,16 @@ struct Mine: View {
             } label: {
                 Image(systemName:"gift.fill")
                     .foregroundColor(.teal)
-                    .font(.system(size: 40))
+                    .font(.system(size: 30))
                 Text("邀请奖品")
                     .foregroundColor(.teal)
-                    .font(.system(size: 30))
+                    .font(.system(size: 25))
                     .multilineTextAlignment(.leading)
                 Spacer()
-                    .frame(width: 150)
+                    .frame(width: 180)
                 Image(systemName: "chevron.forward")
                     .foregroundColor(.teal)
-                    .font(.system(size: 30))
+                    .font(.system(size: 25))
             }
             .frame(height: 50)
 
@@ -111,16 +82,16 @@ struct Mine: View {
             } label: {
                 Image(systemName: "person.fill.questionmark")
                     .foregroundColor(.teal)
-                    .font(.system(size: 40))
+                    .font(.system(size: 30))
                 Text("客服中心")
                     .foregroundColor(.teal)
-                    .font(.system(size: 30))
+                    .font(.system(size: 25))
                     .multilineTextAlignment(.leading)
                 Spacer()
-                    .frame(width: 150)
+                    .frame(width: 180)
                 Image(systemName: "chevron.forward")
                     .foregroundColor(.teal)
-                    .font(.system(size: 30))
+                    .font(.system(size: 25))
             }
             .frame(height: 50)
 
@@ -129,16 +100,16 @@ struct Mine: View {
             } label: {
                 Image(systemName: "text.book.closed.fill")
                     .foregroundColor(.teal)
-                    .font(.system(size: 40))
+                    .font(.system(size: 30))
                 Text("协议说明")
                     .foregroundColor(.teal)
-                    .font(.system(size: 30))
+                    .font(.system(size: 25))
                     .multilineTextAlignment(.leading)
                 Spacer()
-                    .frame(width: 150)
+                    .frame(width: 180)
                 Image(systemName: "chevron.forward")
                     .foregroundColor(.teal)
-                    .font(.system(size: 30))
+                    .font(.system(size: 25))
             }
             .frame(height: 50)
 
@@ -147,19 +118,127 @@ struct Mine: View {
             } label: {
                 Image(systemName: "questionmark.circle")
                     .foregroundColor(.teal)
-                    .font(.system(size: 40))
+                    .font(.system(size: 30))
                 Text("常见问题")
                     .foregroundColor(.teal)
-                    .font(.system(size: 30))
+                    .font(.system(size: 25))
                     .multilineTextAlignment(.leading)
                 Spacer()
-                    .frame(width: 150)
+                    .frame(width: 180)
                 Image(systemName: "chevron.forward")
                     .foregroundColor(.teal)
-                    .font(.system(size: 30))
+                    .font(.system(size: 25))
             }
             .frame(height: 50)
 
+            // 设置
+            Button {
+            } label: {
+                Image(systemName: "gearshape")
+                    .foregroundColor(.teal)
+                    .font(.system(size: 30))
+                Text("软件设置")
+                    .foregroundColor(.teal)
+                    .font(.system(size: 25))
+                    .multilineTextAlignment(.leading)
+                Spacer()
+                    .frame(width: 180)
+                Image(systemName: "chevron.forward")
+                    .foregroundColor(.teal)
+                    .font(.system(size: 25))
+            }
+            .frame(height: 50)
+        }
+    }
+}
+
+
+
+struct Mine: View {
+    var body: some View {
+        VStack{
+            // 用户选项
+            VStack{
+                HStack{
+                    // 头像框
+                    Image("head")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 120, height: 120)
+                        .clipShape(Circle())
+                        .overlay(
+                            Circle().stroke(.teal, lineWidth:2)
+                        )
+                        .shadow(radius: 5)
+                    Spacer()
+                        .frame(width: 30)
+
+                    // 账号昵称
+                    VStack{
+                        Spacer()
+                            .frame(height: 2)
+                        HStack{
+                            VStack{
+                                Text("昵称：")
+                                    .foregroundColor(.teal)
+                                    .font(.system(size: 20))
+                                    .bold()
+                                    .multilineTextAlignment(.leading)
+                                Spacer()
+                                    .frame(height: 3)
+                                Text("账号：")
+                                    .foregroundColor(.teal)
+                                    .font(.system(size: 20))
+                                    .bold()
+                                    .multilineTextAlignment(.leading)
+                            }
+                            VStack{
+                                Text("亚健康终结者")
+                                    .foregroundColor(.teal)
+                                    .font(.system(size: 20))
+                                    .bold()
+                                    .multilineTextAlignment(.leading)
+                                Spacer()
+                                    .frame(height: 3)
+                                Text("1234567890")
+                                    .foregroundColor(.teal)
+                                    .font(.system(size: 20))
+                                    .bold()
+                                    .multilineTextAlignment(.leading)
+                            }
+
+                        }
+                        Spacer()
+                            .frame(height: 11)
+
+                        // 修改个人信息
+                        Button {
+                        } label: {
+                            Image(systemName: "pencil.circle")
+                                .foregroundColor(.white)
+                                .font(.system(size: 20))
+                                .padding([.top, .leading, .bottom], 3.0)
+
+                            Text("修改个人信息")
+                                .foregroundColor(.white)
+                                .font(.system(size: 15))
+                                .padding([.top, .bottom, .trailing], 4.0)
+
+
+                        }
+                        .background(Color.teal)
+                        .cornerRadius(5)
+                    }
+                }
+            }
+            Spacer()
+                .frame(height: 40)
+
+            // 设置
+            Settings()
+
+            Spacer()
+                .frame(height: 90)
         }
     }
 }
